4863f76aabdd52efc8f9d48efc6e36fa9bff8869
Lucas Calbó <lucascalbo@gmail.com>
2023-02-28 11:34:59 +0100

    fuera logo svg
    hay que agregar un logo a la derecha

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo';
 import './index.scss';
 import Loader from 'react-loaders';
 
@@ -39,7 +38,6 @@ const Home = () =>{
                 <h2>Project Manager / Designer / Programmer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-            <Logo />
         </div>
         <Loader type="pacman" />
         </>
